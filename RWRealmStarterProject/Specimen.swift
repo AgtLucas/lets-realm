4bcb6642ce73444984f79bbb45f331c925b96148
Lucas <lucas@agtlucas.com>
2015-06-28 08:51:25 -0300

    Adding Specimen model

---
+++ RWRealmStarterProject/Specimen.swift
@@ -0,0 +1,9 @@
+//
+//  Specimen.swift
+//  RWRealmStarterProject
+//
+//  Created by Lucas da Silva on 6/28/15.
+//  Copyright (c) 2015 Bill Kastanakis. All rights reserved.
+//
+
+import Foundation
