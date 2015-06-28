35daf4e14fbcc0904e0f19ad077ffae9b90ead13
Lucas <lucas@agtlucas.com>
2015-06-28 08:57:56 -0300

    Adding Category model

---
+++ RWRealmStarterProject/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  RWRealmStarterProject
+//
+//  Created by Lucas da Silva on 6/28/15.
+//  Copyright (c) 2015 Bill Kastanakis. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class Category: RLMObject {
+    dynamic var name = ""
+}
